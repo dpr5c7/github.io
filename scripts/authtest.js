895b61846e8f32b9a1ddafb9060bc99a29797e68
dpr5c7 <85196877+dpr5c7@users.noreply.github.com>
2021-06-08 13:28:28 -0500

    Update authtest.js

--- scripts/authtest.js
+++ scripts/authtest.js
@@ -17,7 +17,7 @@
   const btnLogin = document.getElementById('testlogin');
   const btnSignUp = document.getElementById('testsignup');
   const btnLogout = document.getElementById('testlogout');
-Console.log(btnLogin)
+  console.log(btnLogin)
   
  // Add login event
  btnLogin.addEventListener('click', e => {
