240ee22ee7ca186e8663f20c3086f0a70abc03a3
dpr5c7 <85196877+dpr5c7@users.noreply.github.com>
2021-06-08 11:15:50 -0500

    Update authtest.js

--- scripts/authtest.js
+++ scripts/authtest.js
@@ -1,1 +1,22 @@
-asdf
+function () {
+ // Initialize Firebase
+  const config = {
+  apiKey: "AIzaSyBRRG-5SqlUsfo_h1fhaaIeTf_2b5QSJqY",
+  authDomain: "gamervault-fd419.firebaseapp.com",
+  projectId: "gamervault-fd419",
+  storageBucket: "gamervault-fd419.appspot.com",
+  messagingSenderId: "985528869080",
+  appId: "1:985528869080:web:97fa46d30068b7dd56b7ad",
+  measurementId: "G-6W8BG6TNPG"
+}; 
+  firebase.initializeApp(config);
+
+// Get elements
+  const testemail = document.getElementById('testemail');
+  const testpass = document.getElementById('testpass');
+  const btnLogin = document.getElementById('testlogin');
+  const btnSignUp = document.getElementById('testsignup');
+  const btnLogout = document.getElementById('testlogout');
+}
+
+
